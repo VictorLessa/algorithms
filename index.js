97d9e89a1ddf285a2eec7e9b09a546ce435ea35e
unknown <victordsgnr@gmail.com>
2019-09-27 07:55:10 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const list = require('./lista.js');
+
+module.exports = list
---
+++ lista.js
@@ -0,0 +1,37 @@
+'use strict'
+
+class list {
+	constructor(list) {
+		this.list = list
+	}
+	insere (...data) {
+		for (let value of data) {
+			this.list.push(value)
+		}
+		return this.list;
+	}
+	retira(apontador) {
+		let newList = []
+		if (apontador >= this.list.length) {
+			console.log('Err: Posição não existe')
+			return this.list
+		} else {
+			for (let i = 0; i < this.list.length; i++) {
+				if (i != apontador) {
+					newList.push(this.list[i])
+				}
+			}
+		}
+		this.list = newList
+		return this.list		
+	}
+}
+
+module.exports = {
+	insere: function (array, ...data) {
+		return new list(array).insere(...data)
+	},
+	retira: function (array, apontador) {
+		return new list(array).retira(apontador)	
+	} 
+}
