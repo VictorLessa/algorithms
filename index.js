ef0b34452c589dbd70ea731402a094b57f29a1e3
Victor Lessa <victordsgnr@gmail.com>
2019-09-30 07:54:01 -0300

    inserting bubblesort and stock into module

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const list = require('./listAlgorithm.js');
+const bubbleSort = require('./bubbleSort.js');
+const quickSort = require('./quickSort.js');
+const { stack } = require('./stack.js');
 
-module.exports = list
+const modules = {
+    list,
+    bubbleSort,
+    quickSort,
+    stack
+};
+
+module.exports = modules;
